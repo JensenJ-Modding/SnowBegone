f907bf8487c18da689b1ddaf8a14ccf88fb47e52
JensenJ <13812018+JensenJ@users.noreply.github.com>
2025-06-17 09:11:35 +0100

    Snow Under Trees Compat
    Added compatibility for snow under trees.

---
+++ neoforge/src/main/java/net/snowbegone/mixin/neoforge/SnowUnderTreesMixin.java
@@ -0,0 +1,16 @@
+package net.snowbegone.mixin.neoforge;
+
+import bl4ckscor3.mod.snowundertrees.SnowUnderTrees;
+import net.snowbegone.ServerConfig;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+@Mixin(value = SnowUnderTrees.class, priority = 100, remap = false)
+public class SnowUnderTreesMixin {
+
+    @ModifyConstant(method = "isInBuildRangeAndDarkEnough", constant = @Constant(intValue = 10), require = 0)
+    private static int snowbegone$shouldPlaceSnowLayerUnderTrees(int original) {
+        return ServerConfig.snowMeltLightLevel.get() - 1;
+    }
+}
