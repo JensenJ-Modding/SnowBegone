462c2a724a1107ab6eba3068ec15ed71d7a9c739
JensenJ <13812018+JensenJ@users.noreply.github.com>
2025-06-17 09:12:00 +0100

    Linting

--- neoforge/src/main/java/net/snowbegone/mixin/neoforge/SnowUnderTreesMixin.java
+++ neoforge/src/main/java/net/snowbegone/mixin/neoforge/SnowUnderTreesMixin.java
@@ -1,10 +1,11 @@
 package net.snowbegone.mixin.neoforge;
 
-import bl4ckscor3.mod.snowundertrees.SnowUnderTrees;
-import net.snowbegone.ServerConfig;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import bl4ckscor3.mod.snowundertrees.SnowUnderTrees;
+import net.snowbegone.ServerConfig;
 
 @Mixin(value = SnowUnderTrees.class, priority = 100, remap = false)
 public class SnowUnderTreesMixin {
